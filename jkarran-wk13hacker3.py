969c3b61bd6919eb32da8c0c36bb1b20485e3e8e
Jerry Karran <jerrykarran@gmail.com>
2023-07-28 23:21:11 +0000

    added hacker projects

---
+++ jkarran-wk13hacker3.py
@@ -0,0 +1,14 @@
+def split_and_join(line):
+    old = line.split(" ")
+    newstring = ''
+    
+    for i in range (len(old)):
+        newstring += old[i]
+        if i  != len(old)-1:
+            newstring += old[i].join("-")
+    return newstring
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
