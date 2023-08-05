64f8e36b96e68bb38ff6130c6980fe37a137e1fe
Jerry Karran <jerrykarran@gmail.com>
2023-08-05 18:53:36 +0000

    updated advanced code to have correct path name

--- jkarran-week13adv.py
+++ jkarran-week13adv.py
@@ -2,63 +2,16 @@ import os     # import the OS module
 
 directoryDictionary = {}
 
-def printDirectoryData(path='os.getcwd()'):
-    directoryList = os.listdir()
-    
-    for currentFileName in directoryList: # each file in the directory
-        currentFilePath = os.path.abspath(currentFileName)
-        currentFileSize = os.path.getsize(currentFileName) # get the size
-        directoryDictionary[currentFilePath] = currentFileSize # creating an entry in the dictionary
+def printDirectoryData(path=os.path.join(os.getcwd())):
+    directoryList = os.listdir(path) # create a new list of all files and directories
 
-    for p, s in directoryDictionary.items():
+    for currentFileName in directoryList:                       # each file in the directory
+        currentFilePath = str(os.path.abspath(currentFileName)) # get current path
+        fullpath = path + '/' + currentFileName                 # adding the path and current filename
+        currentFileSize = os.path.getsize(fullpath)             # get the size
+        directoryDictionary[fullpath] = currentFileSize  # creating an entry in the dictionary
+        
+    for p, s in directoryDictionary.items():                    # iterating through the dictionary
         print("{'path': '" + p + "', 'size': " + str(s) + '}')
         
-printDirectoryData()
-
-
-
-# def printDirectoryData2(path='os.getcwd()'):
-#     # if path == 'os.getcwd()':
-#     #     directoryList = os.listdir()
-#     # else:
-#     #     directoryList = os.listdir(path)
-    
-    
-#     directoryList = os.listdir(path) 
-#     for currentFileName in directoryList: # each file in the directory  
-#         currentFilePath = os.path.abspath(currentFileName)
-#         currentFileSize = os.path.getsize(currentFileName) # get the size
-#         # currentFileSize2 = os.path.getsize(currentFilePath) # get the size
-#         directoryData[currentFileName] = currentFileSize # creating an entry
-        
-#         # print('filename:', currentFileName + '\nfilenamesize: ' + str(currentFileSize) +  '\n\ncurrentFilePath:', currentFilePath, '\nfilepathsize: ' + str(currentFileSize2) + '\n')
-        
-        
-#         # print("{'path': '" + currentFilePath + "', 'size': " + str(currentFileSize))
-        
-#         # print("{'path': '" + currentFilePath + "', 'namesize': " + str(currentFileSize) + ", 'pathsize': " + str(currentFileSize2) + '}')
-    
-# printDirectoryData2('/home/ec2-user/')
-
-
-
-
-
-def printDirectoryData3(path='os.getcwd()'):
-
-    if path == 'os.getcwd()':
-        directoryList = os.listdir()
-    else:
-        directoryList = os.listdir(path)
-    
-    for currentFileName in directoryList: # each file in the directory
-        currentFilePath = os.path.abspath(currentFileName)
-        currentFileSize = os.path.getsize(currentFileName) # get the size
-        directoryData[currentFilePath] = currentFileSize # creating an entry in the dictionary
-        # print("{'path': '" + currentFilePath + "', 'size': " + str(currentFileSize) + '}')
-
-    for p, s in directoryDictionary.items():
-        print("{'path': '" + p + "', 'size': " + str(s) + '}')
-        
-
-# printDirectoryData3("/home/ec2-user/")
+printDirectoryData('/home/ec2-user/environment/.c9')
