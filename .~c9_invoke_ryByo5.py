ac3d70a7e5a9c9c2a9a3e709fde1dc6aea676cfe
Jerry Karran <jerrykarran@gmail.com>
2023-08-05 22:51:58 +0000

    getting double slash depending on if home or not

---
+++ .~c9_invoke_ryByo5.py
@@ -0,0 +1,37 @@
+import os     # import the OS module
+
+directoryDictionary = {}
+
+def printDirectoryData(path=os.path.join(os.getcwd())):
+    # print('path:', path)
+    recursivePrintDirectory(path)
+    
+    for p, s in directoryDictionary.items():                    # iterating through the dictionary
+        print("{'path': '" + p + "', 'size': " + str(s) + '}')
+
+
+
+def recursivePrintDirectory(path=os.path.join(os.getcwd())):
+    directoryList = os.listdir(path) # create a new list of all files and directories
+
+    for currentFileName in directoryList:                       # each file in the directory
+        if os.path.isdir(currentFileName):                      # if filename is for a directory 
+            fullpath = path +  currentFileName                  # adding the path and current filename
+            # print('in if, folder')
+            recursivePrintDirectory(fullpath)
+        elif len(directoryList) == 0:
+            print("333333")
+            currentFileSize = os.path.getsize(path)
+            directoryDictionary[pathname] = currentFileSize  # creating an entry in the dictionary
+
+        else:
+            print("length:", len(directory))
+            pathname = path + '/' + currentFileName
+            currentFileSize = os.path.getsize(pathname)             # get the size
+            # print('pathname', pathname)
+            
+            directoryDictionary[pathname] = currentFileSize  # creating an entry in the dictionary
+        
+    
+        
+printDirectoryData('/home/ec2-user/environment/')
--- jkarran-week13adv.py
+++ jkarran-week13adv.py
@@ -3,13 +3,12 @@ import os     # import the OS module
 directoryDictionary = {}
 
 def printDirectoryData(path=os.path.join(os.getcwd())):
-    directoryList = os.listdir(path) # create a new list of all files and directories
+    directoryList = os.listdir(path)                            # create a new list of all files and directories
 
     for currentFileName in directoryList:                       # each file in the directory
-        currentFilePath = str(os.path.abspath(currentFileName)) # get current path
         fullpath = path + '/' + currentFileName                 # adding the path and current filename
         currentFileSize = os.path.getsize(fullpath)             # get the size
-        directoryDictionary[fullpath] = currentFileSize  # creating an entry in the dictionary
+        directoryDictionary[fullpath] = currentFileSize         # creating an entry in the dictionary
         
     for p, s in directoryDictionary.items():                    # iterating through the dictionary
         print("{'path': '" + p + "', 'size': " + str(s) + '}')
