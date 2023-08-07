c1ac4d7406af83c2aea0ecbdb3b7ee82f08d1e6b
Jerry Karran <jerrykarran@gmail.com>
2023-08-07 05:38:48 +0000

    receiving error

--- upload_existing_object.py
+++ upload_existing_object.py
@@ -4,3 +4,7 @@ s3 = boto3.client('s3')
 
 with open("test_text.txt", 'rb') as f:
     s3.put_object(Bucket="jkarran-boto3-080623-1756", Key="test_text.txt", Body=f)
+    
+    
+#  with open("test_text.txt", 'rb') as f:
+# FileNotFoundError: [Errno 2] No such file or directory: 'test_text.txt'
