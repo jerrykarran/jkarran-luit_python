98f859962ea7f9cae5ae83a98d37e94fd2ac85a9
Jerry Karran <jerrykarran@gmail.com>
2023-08-10 05:22:51 +0000

    finished thursdays coursework, and medium foundtion article

--- projects/week14/stop_instances.py
+++ projects/week14/stop_instances.py
@@ -1,14 +1,18 @@
 import boto3
-ec2 = boto3.client('ec2')
 
-response = ec2.describe_instance_status()                           # get all instances
-instancesToStop = []                                                #create a list to hold instances to stop
-
-for instance in response["InstanceStatuses"]:                       # check all instances
-    if "InstanceState" in instance:                                 # if an instance exists
-        if instance["InstanceState"]["Name"] == "running":          # check if its running
-            if instance["InstanceId"] != "i-0ed1cf0b0e3fdb30d":    # keep testgin instance running for testing
-                instancesToStop.append(instance["InstanceId"])      # if it's running, add to the list to stop
-                
-if instancesToStop != []:                                           # prevents error if there are no running instances
-    response = ec2.stop_instances(InstanceIds=instancesToStop)      # stop the running instances
+def stop_instances(client):
+    response = client.describe_instance_status()                           # get all instances
+    instancesToStop = []                                                #create a list to hold instances to stop
+    
+    for instance in response["InstanceStatuses"]:                       # check all instances
+        if "InstanceState" in instance:                                 # if an instance exists
+            if instance["InstanceState"]["Name"] == "running":          # check if its running
+                if instance["InstanceId"] != "i-0ed1cf0b0e3fdb30d":    # keep testgin instance running for testing
+                    instancesToStop.append(instance["InstanceId"])      # if it's running, add to the list to stop
+                    
+    if instancesToStop != []:                                           # prevents error if there are no running instances
+        response = client.stop_instances(InstanceIds=instancesToStop)   # stop the running instances
+        
+if __name__ == '__main__':
+    ec2 = boto3.client('ec2')
+    stop_instances(ec2)
