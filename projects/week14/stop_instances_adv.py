0d2555a1b71527d84f0de02436eab9aae27a1269
Jerry Karran <jerrykarran@gmail.com>
2023-08-10 16:33:11 +0000

    changing running to stopped not working

--- projects/week14/stop_instances_adv.py
+++ projects/week14/stop_instances_adv.py
@@ -1,27 +1,25 @@
 import boto3
 
 def stop_instances(client):
-    response = client.describe_instance_status()                           # get all instances
-    instancesToStop = []                                                #create a list to hold instances to stop
+    response = client.describe_instance_status(
+    #     Filters=[
+    #         {
+    #             'Name': 'instance-state-name',
+    #             'Values': [
+    #                 'stopping',
+    #             ]
+    #         },
+    #     ],
+    )                           # get all instances
+    instancesToStop = []
     
     for instance in response["InstanceStatuses"]:                       # check all instances
         if "InstanceState" in instance:                                 # if an instance exists
             if instance["InstanceState"]["Name"] == "running":          # check if its running
                 if instance["InstanceId"] != "i-0ed1cf0b0e3fdb30d":    # keep testgin instance running for testing
                     instancesToStop.append(instance["InstanceId"])      # if it's running, add to the list to stop
-                    
     if instancesToStop != []:                                           # prevents error if there are no running instances
         response = client.stop_instances(InstanceIds=instancesToStop)   # stop the running instances
-        
-# response = ec2.describe_instances()
-
-
-    
-#         if "Tags" in instance:
-#             for tag in instance["Tags"]:
-#                 if tag["Key"] == "Name":
-#                     print(tag["Value"])
-                    
                     
 if __name__ == '__main__':
     ec2 = boto3.client('ec2')
